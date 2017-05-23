0f6c65876c0eeee48e14ea36c4634250ba7b4a3c
Zack YL Shih <zackyl.shih@moxa.com>
2017-05-23 16:05:37 +0800

    Update

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ export MOSQ_DEBIAN_URL=http://repo.mosquitto.org/debian/pool/main/m/mosquitto/mo
 rm -rf workspace
 mkdir -p workspace
 cd workspace
+cp Dockerfile workspace/
 curl -O ${MOSQ_SRC_URL}
 curl -O ${MOSQ_DEBIAN_URL}
 export SRCNAME=`ls mosquitto_*orig*.gz`
@@ -17,7 +18,7 @@ tar zxvf ${SRCNAME}
 tar xvf ${DEBIANNAME} -C `ls | grep mosquitto-*`
 
 # Build amd64 and arm packages
-docker build -f ../Dockerfile -t mosquitto-arm .
+docker build -t mosquitto-arm .
 docker run --name temp-container-name mosquitto-arm /bin/true
 docker cp temp-container-name:/data `pwd`
 # docker run -it -v `pwd`:/data -w /data/`ls | grep mosquitto-*` sanji/mosquitto-dev:armhf /usr/bin/qemu-arm-static sh -c "debuild --no-lintian -us -uc"
